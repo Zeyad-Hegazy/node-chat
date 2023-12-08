67725433539b96af943f661a330bb08d2f931f43
Zeyad Hegazy <zhegazy409@gmail.com>
2023-12-08 17:05:45 +0200

    setup the server

---
+++ index.js
@@ -0,0 +1,57 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const mime = import("mime");
+
+const cache = {};
+
+function send404(response) {
+	response.writeHead(404, { "Content-Type": "text/plain" });
+	response.write("Error 404: resource not found");
+	response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+	response.writeHead(200, {
+		"content-type": mime.lookup(path.basename(filePath)),
+	});
+	response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+	if (cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
+	} else {
+		fs.existsSync(absPath, function (exists) {
+			if (exists) {
+				fs.readFileSync(absPath, function (err, data) {
+					if (err) {
+						send404(response);
+					} else {
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			} else {
+				send404(response);
+			}
+		});
+	}
+}
+
+const server = http.createServer(function (request, response) {
+	let filePath;
+
+	if (request.url === "/") {
+		filePath = "public/index.html";
+	} else {
+		filePath = "public" + request.url;
+	}
+
+	const absPath = "./" + filePath;
+	serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function () {
+	console.log("Server listening on port 3000.");
+});
